a9e08de7eed502745a89d082d0e050133716529c
holtzilya2008 <holtzilya2008@gmail.com>
2015-11-19 13:29:34 +0200

    Ilya 19112015 at 1329 class Question is finished except the zero generator needed for toString

--- app/src/main/java/tastelab/mleshem/tarsisv10/Question.java
+++ app/src/main/java/tastelab/mleshem/tarsisv10/Question.java
@@ -1,5 +1,7 @@
 package tastelab.mleshem.tarsisv10;
 
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -9,6 +11,8 @@ import org.w3c.dom.Text;
 /**
  * Created by ilya on 11/18/15.
  * This class will represent a single question to the participant
+ * This class will be responsible only for representation and storing the data
+ * all the Touch and Click Listeners is set once in the experiment class
  */
 public class Question {
 /* -------------------------------- fields *-------------------------------- */
@@ -31,6 +35,7 @@ public class Question {
     private SeekBar seekBar;
     private int answer;
     private boolean isAnswered;
+    private boolean isActive;
 
 /* ---------------------------- Constant Values ---------------------------- */
 
@@ -41,6 +46,10 @@ public class Question {
     private static final String TARSIS_WORD = String.valueOf(R.string.tarsis_word);
     private static final String QUESTION_STRONG = String.valueOf(R.string.strong_question);
     private static final String QUESTION_TASTY = String.valueOf(R.string.tasty_question);
+
+    private static final int INIT_ANSWER = -1;
+    private static final int DEFAULT_PROGRESS = R.integer.default_progress;
+    private static final int MAX_PROGRESS = R.integer.max_progress;
 
 /* ---------------------------- DEBUG Environment -------------------------- */
 
@@ -62,28 +71,99 @@ public class Question {
         this.order = order;
         this.about = about;
         this.tarsisType = tarsisType;
+        this.isActive = false;
 
-
+        loadQuestionEnvironment();
     }
 
 /* ----------------------------- Public Methods ---------------------------- */
 
+    public void Show(){
+        updateEnvironment();
+        this.questionProperties.setVisibility(View.VISIBLE);
+        this.questionText.setVisibility(View.VISIBLE);
+        this.answerText.setVisibility(View.VISIBLE);
+        this.seekBar.setVisibility(View.VISIBLE);
+        this.isActive = true;
+    }
+
+    public void Close(){
+        this.questionProperties.setVisibility(View.GONE);
+        this.questionText.setVisibility(View.GONE);
+        this.answerText.setVisibility(View.GONE);
+        this.seekBar.setVisibility(View.GONE);
+        this.isActive = false;
+    }
+
+    public void SetAnswer(int ans){
+        this.answer = ans;
+        this.isAnswered = true;
+    }
+
+    public boolean isAnswered(){
+        return isAnswered;
+    }
+
+    public boolean isActive(){
+        return isActive;
+    }
+
 /* ----------------------------- Object Methods ---------------------------- */
 
-/* ---------------------------- Private Methods ---------------------------- */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Question question = (Question) o;
+
+        return qid == question.qid;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return qid;
+    }
+
+    @Override
+    public String toString() {
+        return ""+answer; //TODO make a number of zeros generator
+    }
+
+    /* ---------------------------- Private Methods ---------------------------- */
 
     private void loadQuestionEnvironment(){
-        /*
-        private TextView questionText;
-        private TextView questionProperties;
-        private TextView answerText; // X/100
-        private SeekBar seekBar;
-        private int answer;
-        private boolean isAnswered;
-        */
+        this.questionText = (TextView)activity.findViewById(R.id.questionText);
+        this.questionProperties = (TextView)activity.findViewById(R.id.qproperties);
+        if(this.about == STRONG) {
+            this.answerText = (TextView) activity.findViewById(R.id.strongText);
+            this.seekBar = (SeekBar) activity.findViewById(R.id.strongBar);
+        }else if(this.about == TASTY){
+            this.answerText = (TextView)activity.findViewById(R.id.tastyText);
+            this.seekBar = (SeekBar)activity.findViewById(R.id.tastyBar);
+        }
+        this.answer = INIT_ANSWER;
+        this.isAnswered = false;
+    }
 
-
-
+    private void updateEnvironment(){
+        if(about == STRONG) {
+            this.questionText.setText(QUESTION_STRONG + "?");
+        }else if(about == TASTY){
+            this.questionText.setText(QUESTION_TASTY + "?");
+        }
+        this.questionProperties.setText(SIDRA_WORD + " " + this.order "   " +
+                                        TARSIS_WORD + " " + this.tarsisFake);
+        if(isAnswered) {
+            seekBar.setProgress(answer);
+            answerText.setText(answer+"/"+MAX_PROGRESS);
+        }else{
+            seekBar.setProgress(DEFAULT_PROGRESS);
+            answerText.setText(DEFAULT_PROGRESS + "/" + MAX_PROGRESS);
+            Log.d(TAG,"Question" + qid + " :Is not been answered yet, " +
+                    "setting the default values");
+        }
     }
 
 } // End of Class Question ------------------------------------------------- //
